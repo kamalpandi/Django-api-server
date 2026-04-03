f242bc1b0e94ddae5895a9d475a38e6614be9b71
kamal <kamalpandi123@gmail.com>
2026-04-03 12:49:56 +0530

    weather app migration

--- todo_site/settings.py
+++ todo_site/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ["*", "vibi.pythonanywhere.com", "127.0.0.1:5500", "localhost"]
 INSTALLED_APPS = [
     "todo",
     "weather",
+    "corsheaders",
     "pic_to_ASCII",
     "django.contrib.admin",
     "django.contrib.auth",
@@ -41,10 +42,19 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 ]
+# Option A: Allow all origins (Easiest for local development)
+CORS_ALLOW_ALL_ORIGINS = True
 
+# Option B: Or explicitly allow your React dev server
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:5173",
+#     "http://localhost:3000",
+#     "http://127.0.0.1:5173",
+# ]
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-"django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -126,7 +136,7 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
